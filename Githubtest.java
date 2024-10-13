abe86816c7cd539a46aa96968ea713f80db15c16
Jinan-Almutairi <jinanalmutairi1122@gmail.com>
2024-10-13 18:07:54 +0300

    Add files via upload

---
+++ Githubtest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubtest;
+
+
+public class Githubtest {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World!");
+        System.out.println("My Name is; Jinan");
+    }
+}
